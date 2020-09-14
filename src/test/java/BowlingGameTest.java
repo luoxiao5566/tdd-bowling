0b5fa4a6fafadfadd7d6d6ebd2c56222db0ff0c1
xiao luo <xiao.luo@thoughtworks.com>
2020-09-14 22:19:20 +0800

    [luoxiao] implement should return score when sum total score given bowling game over of complex situation

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -70,4 +70,25 @@ public class BowlingGameTest {
         int result = bowlingGame.sum_total_score(competition_record);
         assertEquals(score,result);
     }
+
+    @Test
+    void should_return_score_when_sum_total_score_given_bowling_game_over_of_complex_situation(){
+
+        BowlingGame bowlingGame = new BowlingGame();
+
+        int score1 = 136;
+        int score2 = 163;
+        int score3 = 146;
+
+        int [] competition_record1 = {5,5,3,4,10,7,3,6,3,4,4,7,3,10,8,2,5,3};
+        int [] competition_record2 = {5,5,3,4,10,7,3,6,3,4,4,7,3,10,8,2,10,10,10};
+        int [] competition_record3 = {5,5,3,4,10,7,3,6,3,4,4,7,3,10,8,2,6,4,7};
+
+        int result1 = bowlingGame.sum_total_score(competition_record1);
+        int result2 = bowlingGame.sum_total_score(competition_record2);
+        int result3 = bowlingGame.sum_total_score(competition_record3);
+        assertEquals(score1,result1);
+        assertEquals(score2,result2);
+        assertEquals(score3,result3);
+    }
 }
