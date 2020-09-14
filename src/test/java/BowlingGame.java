252bc21aaaec6ef18713a26f8a4d641547f5de13
xiao luo <xiao.luo@thoughtworks.com>
2020-09-14 20:38:27 +0800

    [luoxiao] implement should return score when Summation given twice Pitching less than 10 pin

---
+++ src/test/java/BowlingGame.java
@@ -0,0 +1,5 @@
+public class BowlingGame {
+    public Integer twice_Pitching_less_than_10_pin(int number_of_first_pin, int number_of_second_pin) {
+        return number_of_first_pin+number_of_second_pin;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class BowlingGameTest {
+    @Test
+    void should_return_score_when_Summation_given_twice_Pitching_less_than_10_pin(){
+
+        BowlingGame bowlingGame = new BowlingGame();
+
+        Integer score = 7;
+
+        Integer result = bowlingGame.twice_Pitching_less_than_10_pin(4,3);
+        assertEquals(score,result);
+    }
 }
